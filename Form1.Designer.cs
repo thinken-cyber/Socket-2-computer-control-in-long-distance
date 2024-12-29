3d4e9f95fcd346b36c6c780cce3d3cfb27db99ac
thinken-cyber <ducthinhn249@gmail.com>
2024-12-29 10:02:56 +0700

    Initial commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,128 @@
+﻿namespace dieukhientuxa_tatmay_client
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtServerIp = new System.Windows.Forms.TextBox();
+            this.txtPort = new System.Windows.Forms.TextBox();
+            this.txtCommand = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnSendCommand = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtServerIp
+            // 
+            this.txtServerIp.Location = new System.Drawing.Point(173, 61);
+            this.txtServerIp.Name = "txtServerIp";
+            this.txtServerIp.Size = new System.Drawing.Size(380, 26);
+            this.txtServerIp.TabIndex = 0;
+            // 
+            // txtPort
+            // 
+            this.txtPort.Location = new System.Drawing.Point(173, 141);
+            this.txtPort.Name = "txtPort";
+            this.txtPort.Size = new System.Drawing.Size(380, 26);
+            this.txtPort.TabIndex = 1;
+            // 
+            // txtCommand
+            // 
+            this.txtCommand.Location = new System.Drawing.Point(173, 223);
+            this.txtCommand.Name = "txtCommand";
+            this.txtCommand.Size = new System.Drawing.Size(380, 26);
+            this.txtCommand.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(21, 61);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(70, 20);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "ServerIP";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(21, 140);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(38, 20);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Port";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(21, 219);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(82, 20);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Command";
+            // 
+            // btnSendCommand
+            // 
+            this.btnSendCommand.Location = new System.Drawing.Point(281, 324);
+            this.btnSendCommand.Name = "btnSendCommand";
+            this.btnSendCommand.Size = new System.Drawing.Size(167, 47);
+            this.btnSendCommand.TabIndex = 8;
+            this.btnSendCommand.Text = "Send";
+            this.btnSendCommand.UseVisualStyleBackColor = true;
+            this.btnSendCommand.Click += new System.EventHandler(this.btnSendCommand_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnSendCommand);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtCommand);
+            this.Controls.Add(this.txtPort);
+            this.Controls.Add(this.txtServerIp);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtServerIp;
+        private System.Windows.Forms.TextBox txtPort;
+        private System.Windows.Forms.TextBox txtCommand;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnSendCommand;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Net.Sockets;
+using System.Text;
+using System.Windows.Forms;
+
+namespace dieukhientuxa_tatmay_client
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSendCommand_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                // Kiểm tra dữ liệu đầu vào
+                string serverIp = txtServerIp.Text.Trim();
+                if (string.IsNullOrEmpty(serverIp))
+                {
+                    MessageBox.Show("Vui lòng nhập địa chỉ IP của Server!", "Lỗi đầu vào");
+                    return;
+                }
+
+                if (!int.TryParse(txtPort.Text.Trim(), out int port) || port <= 0 || port > 65535)
+                {
+                    MessageBox.Show("Vui lòng nhập port hợp lệ (1-65535)!", "Lỗi đầu vào");
+                    return;
+                }
+
+                string command = txtCommand.Text.Trim();
+                if (string.IsNullOrEmpty(command))
+                {
+                    MessageBox.Show("Vui lòng nhập lệnh cần gửi!", "Lỗi đầu vào");
+                    return;
+                }
+
+                // Gửi lệnh đến Server
+                using (TcpClient client = new TcpClient())
+                {
+                    client.Connect(serverIp, port); // Kết nối tới Server
+                    using (NetworkStream stream = client.GetStream())
+                    {
+                        byte[] data = Encoding.UTF8.GetBytes(command);
+                        stream.Write(data, 0, data.Length); // Gửi dữ liệu
+
+                        MessageBox.Show($"Lệnh '{command}' đã được gửi thành công!", "Thông báo");
+                    }
+                }
+            }
+            catch (SocketException ex)
+            {
+                MessageBox.Show($"Không thể kết nối tới Server: {ex.Message}", "Lỗi kết nối");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Đã xảy ra lỗi: {ex.Message}", "Lỗi");
+            }
+        }
+    }
+}
